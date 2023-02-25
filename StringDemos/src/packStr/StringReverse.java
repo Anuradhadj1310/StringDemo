bc6f7efc11b9ee91a72c396671894f1046112530
Anuradhadj1310 <jagtapsharayad@gmail.com>
2023-02-25 15:05:13 +0530

    name reversalnot shadow reversal yet

--- StringDemos/src/packStr/StringReverse.java
+++ StringDemos/src/packStr/StringReverse.java
@@ -9,6 +9,35 @@ public class StringReverse {
 		//output must be: shihsA rakehoP
 		String strFName="Is",strLName="Friend";
 		String revStr="",fname="";
+		char ch;
+		String splitStr[]=name.split("\\s");
+		
+		System.out.println(splitStr[0]);
+		System.out.println("**********************"+splitStr[1]);
+		
+		for(int i=0;i<splitStr.length;i++)
+			//for(int i=splitStr.length;i>=0;i--)
+		{
+			for(int j=0;j<splitStr[i].length();j++)
+			{
+				ch=splitStr[i].charAt(j);
+			
+				fname=ch+fname;
+			}
+			fname=" "+fname;
+		}
+		//String fname,lname;
+		/*
+		for(int i=0;i<strFName.length();i++)
+		{
+			ch=strFName.charAt(i);
+			fname=ch+fname;
+			
+		}
+		*/
+		System.out.println("Original :"+strFName+" "+strLName);
+		System.out.println("Reveresed: "+fname);
+
 	}
 
 }
