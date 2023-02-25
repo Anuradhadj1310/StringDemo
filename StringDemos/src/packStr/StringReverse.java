4619d4a5e1661d41b002733956028b4fe24458c4
Anuradhadj1310 <jagtapsharayad@gmail.com>
2023-02-25 16:48:38 +0530

    final commit forstring reversal

--- StringDemos/src/packStr/StringReverse.java
+++ StringDemos/src/packStr/StringReverse.java
@@ -5,9 +5,6 @@ public class StringReverse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String name="Ashish Pohekar";
-		//input : Ashish Pohekar
-		//output must be: shihsA rakehoP
-		//String strFName="Is",strLName="Friend";
 		String revStr="",fname="";
 		char ch;
 		String splitStr[]=name.split("\\s");
@@ -15,9 +12,6 @@ public class StringReverse {
 		System.out.println(splitStr[0]);
 		System.out.println("**********************"+splitStr[1]);
 		System.out.println("**********************"+splitStr.length);
-		//for(int i=0;i<splitStr.length;i++)
-		//{
-		
 			for(int i=splitStr.length-1;i>=0;i--)
 			{{
 					int len=splitStr[i].length();
@@ -31,17 +25,6 @@ public class StringReverse {
 				fname=" "+fname;
 			}
 		}
-		//String fname,lname;
-		/*
-		for(int i=0;i<strFName.length();i++)
-		{
-			ch=strFName.charAt(i);
-			fname=ch+fname; 
-			//net disconnected
-			
-		}
-		*/
-		//System.out.println("Original :"+strFName+" "+strLName);
 		System.out.println("Reveresed: "+fname);
 
 	}
